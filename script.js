9b62263a9415df76e00e4aa8390996108caf6863
Pamidimarri vijay sankar <sankerpamidimarri@gmail.com>
2024-11-25 13:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,13 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let arrval=document.getElementsByClassName("price");
+let ct=0;
+for(let i=0;i<arrval.length;i++){
+	ct+=parseFloat(arrval[i].textContent || 0);
+}
+	console.log(ct);
+document.querySelector("#val").textContent=ct;
 };
 
 getSumBtn.addEventListener("click", getSum);
